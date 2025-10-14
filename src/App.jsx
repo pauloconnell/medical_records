ddcd9356a885a3e0ea05c679be0ae52c3c308aaa
Paul O'Connell <p_ollie@hotmail.com>
2025-10-14 16:32:51 -0700

    set up app.jsx to hold data and components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import "./App.css";
 import "h8k-components";
 import Search from "./components/Search";
 import Records from "./components/Records";
+import medical_records from "./medicalRecords";
 
 const title = "Patient Medical Records";
 
+
 const App = () => {
+
+  console.log({medical_records})
+  const [id, setId] = React.useState(0);
+  
+
   return (
     <div className="App">
       <h8k-navbar header={title}></h8k-navbar>
       <div className="content">
-        <Search />
-        <Records />
+        <Search records={medical_records} setId={setId} id={id}/>
+        <Records records={medical_records} id={id} />
+       
       </div>
     </div>
   );

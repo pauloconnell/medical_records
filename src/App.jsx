50bae03321e52eab1d2fd55e3e89c42e5bb997f8
Paul O'Connell <p_ollie@hotmail.com>
2025-10-15 09:49:37 -0700

    Update: css - Header looks better with small caps

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,31 @@ const title = "Patient Medical Records";
 
 const App = () => {
 
-  console.log({medical_records})
+  //console.log({medical_records})
   const [id, setId] = useState(0);
   const [selectionMade, setSelectionMade] = useState(false);
 
   return (
-    <div className="App">
-      <h8k-navbar header={title}></h8k-navbar>
-      <div className="content">
-        <Search records={medical_records} setId={setId} id={id} selectionMade={selectionMade} setSelectionMade={setSelectionMade}/>
-        <Records records={medical_records} setId={setId} id={id} selectionMade={selectionMade} />
-       
-      </div>
-    </div>
+     <div className="App">
+        <header className="header layout-row align-items-center justify-content-center">
+           <div id="title">{title}</div>
+        </header>
+        <div className="content">
+           <Search
+              records={medical_records}
+              setId={setId}
+              id={id}
+              selectionMade={selectionMade}
+              setSelectionMade={setSelectionMade}
+           />
+           <Records
+              records={medical_records}
+              setId={setId}
+              id={id}
+              selectionMade={selectionMade}
+           />
+        </div>
+     </div>
   );
 };
 
